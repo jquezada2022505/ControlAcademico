4e02661193af665b71ffb1be0a4ca96bf6ec1a9b
jquezada2022505 <jquezada-2022505@kinal.edu.gt>
2024-02-13 17:31:00 -0600

    Se agrego feature/estudiante

---
+++ controllers/students.controllers.js
@@ -0,0 +1,78 @@
+const bcryptjs = require('bcryptjs');
+const Student = require('../models/student');
+
+const studentGet = async (req, res = response) => {
+    const { limite, desde } = req.query;
+    const query = { estado: true };
+
+    const [total, student] = await Promise.all([
+        Student.countDocuments(query),
+        Student.find(query)
+            .skip(Number(desde))
+            .limit(Number(limite))
+    ]);
+
+    res.status(200).json({
+        total,
+        student
+    });
+}
+
+const getStudentById = async (req, res) => {
+    const { id } = req.params;
+    const student = await Student.findOne({ _id: id });
+
+    res.status(200).json({
+        student
+    });
+}
+
+const putStudent = async (req, res = response) =>{
+    const {id } = req.params;
+    const {_id, password, google, correo, ...resto} = req.body
+
+    if(password){
+        const salt = bcryptjs.genSaltSync();
+        resto.password = bcryptjs.hashSync(password, salt);
+    }
+
+    const student = await Student.findByIdAndUpdate(id, resto);
+
+    res.status(200).json({
+        msg: 'Estudiante Actualizado Exitosamente',
+        student
+    });
+}
+
+const studentDelete = async (req, res) =>{
+    const{id} = req.params;
+    const student = await Student.findByIdAndUpdate(id, {estado: false});
+
+    res.status(200).json({
+        msg: 'Estudiante eliminado exitosamente',
+        student
+    });
+}
+
+const studentPost = async (req, res) => {
+    const { nombre, correo, password, curso, role } = req.body;
+    const student = new Student({ nombre, correo, password, curso, role});
+
+    const salt = bcryptjs.genSaltSync();
+    console.log(password);
+    student.password = bcryptjs.hashSync(password, salt);
+
+    await student.save();
+    console.log({ nombre, correo, password, curso, role })
+    res.status(202).json({
+        student
+    });
+}
+
+module.exports = {
+    studentPost,
+    getStudentById,
+    putStudent,
+    studentGet,
+    studentDelete
+}
---
+++ models/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors =  require('cors');
+const {dbConnection} = require('../db/config')
+
+class Server {
+
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+        this.studentPath = '/api/student';
+        this.animalesPath = '/api/teacher';
+
+        this.conectarDB();
+        this.middlewares();
+        this.routes();
+    }
+
+    async conectarDB(){
+        await dbConnection();
+    }
+
+    middlewares(){
+        this.app.use(express.static('public'));
+        this.app.use(cors());
+        this.app.use(express.json());
+    }
+
+    routes(){
+        this.app.use(this.studentPath, require('../routes/student.routes'));
+        this.app.use(this.animalesPath, require('../routes/teacher.routes'));
+    }
+
+    listen(){
+        this.app.listen(this.port, () =>{
+            console.log('Servidor ejecutandose y escuchandose en el puerto:', this.port)
+        });
+    }
+}
+
+module.exports = Server;
